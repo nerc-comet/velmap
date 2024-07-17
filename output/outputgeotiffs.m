122e1f144dcdc5320dd530097534f72a2bde8f78
Dehua Wang <93398659+eedwa@users.noreply.github.com>
2024-07-17 14:42:57 +0800

    Update outputgeotiffs.m

--- output/outputgeotiffs.m
+++ output/outputgeotiffs.m
@@ -8,11 +8,11 @@
 % Modified by Dehua Wang, University of Leeds, 04/03/2023
 %=================================================================
 
-load precrash.mat
+load examples/ATF/output/smf-1.10/precrash.mat
 %% read in files produced by insarfit and the original interferograms
-load insar_1lk.mat
-load insarfit2.mat
-mkdir geotiffs-1.5-m
+load examples/ATF/output/smf-1.10/insar_1lk.mat
+load examples/ATF/output/smf-1.10/insarfit2.mat
+mkdir geotiffs-1.1-m
 ninsar=length(insar);
 
 for i=1:ninsar
@@ -23,7 +23,7 @@ for i=1:ninsar
     [vstd,R]=geotiffread(stackmapname);
     
     %write velocities referenced to Eurasia
-    filename = sprintf("%s%d%s","geotiffs-1.5-m/vel_eurasiaref_frame_",i,".tif");
+    filename = sprintf("%s%d%s","geotiffs-1.1-m/vel_eurasiaref_frame_",i,".tif");
     outgrid = insarfit2(i).ratemap+insarfit2(i).resmap; %this is the original interferogram with atmosphere and orbital correction terms applied to put it into geocoded coordinates.
 %   outgrid = insar(i).stackmap-insarfit2(i).orbmap;
     %outgrid(isnan(outgrid))= -9999;
@@ -31,24 +31,24 @@ for i=1:ninsar
     geotiffwrite(filename,outgrid,R);
 
     %write vstd
-    filename = sprintf("%s%d%s","geotiffs-1.5-m/vstd_",i,"vstd.geo.tif");
+    filename = sprintf("%s%d%s","geotiffs-1.1-m/vstd_",i,"vstd.geo.tif");
     %vstd(isnan(vstd))= -9999;
     geotiffwrite(filename,vstd,R);  %just rewriting what we read in to make life easier later on
 
     %write orbmap
-    filename = sprintf("%s%d%s","geotiffs-1.5-m/orbmap_",i,".tif");
+    filename = sprintf("%s%d%s","geotiffs-1.1-m/orbmap_",i,".tif");
     outgrid = insarfit2(i).orbmap;
     %outgrid(isnan(outgrid))= -9999;
     geotiffwrite(filename,outgrid,R);
     
     %write atmmap
-    filename = sprintf("%s%d%s","geotiffs-1.5-m/atmmap_",i,".tif");
+    filename = sprintf("%s%d%s","geotiffs-1.1-m/atmmap_",i,".tif");
     outgrid = insarfit2(i).atmmap;
     %outgrid(isnan(outgrid))= -9999;
     geotiffwrite(filename,outgrid,R);
     
     %write resmap
-    filename = sprintf("%s%d%s","geotiffs-1.5-m/resmap_",i,".tif");
+    filename = sprintf("%s%d%s","geotiffs-1.1-m/resmap_",i,".tif");
     outgrid = insarfit2(i).resmap;
     outgrid = -outgrid;
     %outgrid(isnan(outgrid))= -9999;
@@ -56,7 +56,7 @@ for i=1:ninsar
     
     
     %write modeled los rate
-    filename = sprintf("%s%d%s","geotiffs-1.5-m/modellosmap_",i,".tif");
+    filename = sprintf("%s%d%s","geotiffs-1.1-m/modellosmap_",i,".tif");
     outgrid = insarfit2(i).ratemap;
     outgrid = -outgrid;
     %outgrid(isnan(outgrid))= -9999;
